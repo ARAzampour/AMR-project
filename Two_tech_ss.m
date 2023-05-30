ab6a92452726d9cf99a5179fa002163c2a09a7f7
ARAzampour <ali.r.azampour@gmail.com>
2023-05-30 10:11:38 +0200

    these are the changes in the model

---
+++ Two_tech_ss.m
@@ -0,0 +1,292 @@
+function [policy_choice_o,v_new_o,v_new_resh_o,dist_o,trans_matrix_n,...
+    policy_choice_n,v_new_n,v_new_resh_n,dist_n,trans_matrix_o,...
+    age_g,a_grid,a_prob,pi_contemp_new,p_E,m_of_firms_new] = ...
+    Two_tech_ss(a_grow,alpha,a_bar,beta,c_of_a,a_lamb,p_e,a_num_g,age_num,max_iter,...
+    v_tol,dist_tol,fco,e_p,d_0,c_of_e,dem_tol,tech_dist)
+
+a_grid  =  expinv(linspace(0,0.999,a_num_g),a_lamb);
+a_cdf   = expcdf(a_grid,a_lamb);
+a_prob  = [a_cdf(2:a_num_g) 1]-a_cdf;
+
+max_iter_price      = floor(max_iter/100);
+max_iter_measure    = max_iter_price;
+ 
+age_g   = (linspace(0,age_num-1,age_num))';
+
+
+
+policy_choice_n   = zeros(age_num*a_num_g,a_num_g);
+policy_choice_o   = zeros(age_num*a_num_g,a_num_g);
+
+
+%%% Here we will have endogenous entry: this means that as long as value of
+%%% entrant - the cost of entry is positive there should be entrants. To
+%%% solve it I will loop over the whole soltuion till I find the
+%%% equilibrium price satisfying this condition; first I start with p_E = 1
+
+
+p_E = 1;
+
+pi_contemp_new      = ((1+a_grid).*(alpha*p_E/p_e)^alpha.*(1/(1+a_grow)).^age_g)...
+    .^(1/(1-alpha))*(1-alpha);
+
+cap_contemp_new     = (((1+a_grid).*(alpha*p_E/p_e)^alpha.*(1/(1+a_grow)).^age_g)...
+    .^(1/(1-alpha)))';
+
+%%% I also consider the remainder of the firms that have not transitioned
+
+pi_contemp_old      = ((1+a_grid)/tech_dist.*(alpha*p_E/p_e)^alpha.*(1/(1+a_grow)).^age_g)...
+    .^(1/(1-alpha))*(1-alpha);
+
+cap_contemp_old     = (((1+a_grid)/tech_dist.*(alpha*p_E/p_e)^alpha.*(1/(1+a_grow)).^age_g)...
+    .^(1/(1-alpha)))';
+
+
+%%% here I'm adding fixed cost of operation to implement exit decision: it
+%%% should be calibrated
+
+pi_contemp_new      = pi_contemp_new - fco;
+pi_contemp_old      = pi_contemp_old - fco;
+
+%%% those with negative contemporary profit will exit
+pi_contemp_neg_new  = pi_contemp_new<0;
+pi_contemp_new(pi_contemp_neg_new) = 0;
+
+pi_contemp_neg_old  = pi_contemp_old<0;
+pi_contemp_old(pi_contemp_neg_old) = 0;
+
+
+
+
+v_of_new = pi_contemp_new;
+v_of_old = pi_contemp_old;
+
+
+%%%% also to have the entry and exit equal to each other the measure of the
+%%%% firms should be the correct number
+
+m_of_firms_new  = 1;
+m_of_firms_old  = 1;
+
+
+for h=1:1:max_iter_measure
+    for k=1:1:max_iter_price
+        pi_contemp_new      = ((1+a_grid).*(alpha*p_E/p_e)^alpha.*(1/(1+a_grow)).^age_g)...
+            .^(1/(1-alpha))*(1-alpha);
+        
+        cap_contemp_new     = (((1+a_grid).*(alpha*p_E/p_e)^alpha.*(1/(1+a_grow)).^age_g)...
+            .^(1/(1-alpha)))';
+    
+        pi_contemp_new      = pi_contemp_new - fco;
+    
+        pi_contemp_neg_new  = pi_contemp_new<0;
+        pi_contemp_new(pi_contemp_neg_new) = 0;
+
+        pi_contemp_old      = ((1+a_grid)/tech_dist.*(alpha*p_E/p_e)^alpha.*(1/(1+a_grow)).^age_g)...
+            .^(1/(1-alpha))*(1-alpha);
+        
+        cap_contemp_old     = (((1+a_grid)/tech_dist.*(alpha*p_E/p_e)^alpha.*(1/(1+a_grow)).^age_g)...
+            .^(1/(1-alpha)))';
+        pi_contemp_old      = pi_contemp_old - fco;
+        
+        pi_contemp_neg_old  = pi_contemp_old<0;
+        pi_contemp_old(pi_contemp_neg_old) = 0;
+
+
+
+        for i1=1:1:max_iter
+        
+            v_p_n = v_of_new';
+            v_p_n(:,1:age_num-1)  = v_p_n(:,2:age_num);
+            v_p_n(:,age_num)      = 0;            %%% setting the
+                                                %%% value of those who don't adopt
+                                                %%% at the highest age to zero; it
+                                                %%% should also be noted that if a
+                                                %%% firm is not adopting, in the
+                                                %%% next period it would get the
+                                                %%% value of v(age+1,a_i)
+        
+        
+        
+            v_p_n_vec     = (v_p_n(:));
+            v_p_n_expand  = kron(v_p_n_vec,ones(a_num_g,1));
+            v_n_adopt     = kron(ones(age_num,a_num_g),v_of_new(1,:));
+            v_n_adopt_p   = v_n_adopt';
+            v_n_adopt_vec = v_n_adopt_p(:);        
+        
+            v_n_best      = max(v_n_adopt_vec-c_of_a*(1)...
+                ,v_p_n_expand);
+        
+            v_n_best_resh = (reshape(v_n_best,a_num_g,age_num*a_num_g))';
+        
+             
+            policy_choice_n = 1-(v_n_best_resh==v_p_n_vec);
+        
+            %%% those who couldn't pay the fco should exit
+            exiting_firm_n    = pi_contemp_neg_new';
+            exit_vec_n        = exiting_firm_n(:);
+            policy_choice_n   = (1-exit_vec_n).*policy_choice_n;
+        
+            v_new_n       = v_n_best_resh*a_prob';
+            v_new_resh_n  = (reshape(v_new_n,a_num_g,age_num))';
+            v_new_resh_n  = pi_contemp_new + beta*v_new_resh_n;
+
+            v_new_resh_n(pi_contemp_neg_new) = 0;
+        
+            error       = max(abs(v_of_new-v_new_resh_n),[],"all");
+        
+            if error<v_tol
+                
+                fprintf("value function of new converged in %4.1f periods\n",i1);
+                break;
+            end
+        
+            v_of_new = v_new_resh_n;
+        end
+        
+        for i2=1:1:max_iter
+            v_p_o = v_of_old';
+            v_p_o(:,1:age_num-1)  = v_p_o(:,2:age_num);
+            v_p_o(:,age_num)      = 0;            %%% setting the
+                                                %%% value of those who don't adopt
+                                                %%% at the highest age to zero; it
+                                                %%% should also be noted that if a
+                                                %%% firm is not adopting, in the
+                                                %%% next period it would get the
+                                                %%% value of v(age+1,a_i)
+        
+        
+        
+            v_p_o_vec     = (v_p_o(:));
+            v_p_o_expand  = kron(v_p_o_vec,ones(a_num_g,1));
+            v_o_adopt     = kron(ones(age_num,a_num_g),v_of_old(1,:));
+            v_o_adopt_p   = v_o_adopt';
+            v_o_adopt_vec = v_o_adopt_p(:);        
+        
+            v_o_best      = max(v_o_adopt_vec-c_of_a*(1)...
+                ,v_p_o_expand);
+        
+            v_o_best_resh = (reshape(v_o_best,a_num_g,age_num*a_num_g))';
+        
+             
+            policy_choice_o = 1-(v_o_best_resh==v_p_o_vec);
+        
+            %%% those who couldn't pay the fco should exit
+            exiting_firm_o    = pi_contemp_neg_old';
+            exit_vec_o        = exiting_firm_o(:);
+            policy_choice_o   = (1-exit_vec_o).*policy_choice_o;
+        
+            v_new_o       = v_o_best_resh*a_prob';
+            v_new_resh_o  = (reshape(v_new_o,a_num_g,age_num))';
+            v_new_resh_o  = pi_contemp_old + beta*v_new_resh_o;
+
+            v_new_resh_o(pi_contemp_neg_old) = 0;
+        
+            error       = max(abs(v_of_old-v_new_resh_o),[],"all");
+        
+            if error<v_tol
+                
+                fprintf("value function of old converged in %4.1f periods\n",i2);
+                break;
+            end
+        
+            v_of_old = v_new_resh_o;
+        end
+        
+        %%
+        %%% Here I derive the transition matrix; first those who don't adopt go to
+        %%% the same technology state with a higher age, those who adopt go the
+        %%% technology they adopt with the age zero meaning we have:
+        
+        trans_matrix_n    = zeros(a_num_g*age_num,a_num_g*age_num);
+        trans_matrix_n(:,1:a_num_g) = policy_choice_n.*repmat(a_prob,age_num*a_num_g,1);
+        prob_of_naot_n    = sum((1-policy_choice_n).*repmat(a_prob,age_num*a_num_g,1),2);
+        state_if_naot_n   =  kron([1:age_num-1],ones(1,a_num_g))*age_num*a_num_g^2+...
+            [1:(age_num-1)*a_num_g]+kron(ones(1,age_num-1),[0:a_num_g-1])*age_num*a_num_g;
+        trans_matrix_n(state_if_naot_n) = 1*prob_of_naot_n(1:(age_num-1)*a_num_g);
+
+        trans_matrix_o    = zeros(a_num_g*age_num,a_num_g*age_num);
+        trans_matrix_o(:,1:a_num_g) = policy_choice_o.*repmat(a_prob,age_num*a_num_g,1);
+        prob_of_naot_o    = sum((1-policy_choice_o).*repmat(a_prob,age_num*a_num_g,1),2);
+        state_if_naot_o   =  kron([1:age_num-1],ones(1,a_num_g))*age_num*a_num_g^2+...
+            [1:(age_num-1)*a_num_g]+kron(ones(1,age_num-1),[0:a_num_g-1])*age_num*a_num_g;
+        trans_matrix_o(state_if_naot_o) = 1*prob_of_naot_o(1:(age_num-1)*a_num_g);
+        
+        %%% also those who are at the last period would die if they don't adopt to
+        %%% any technology and a new firm would enter with a random technology
+        %%% meaning for those states with the highest age the transition would be
+        %%% randomly to age 0 and tech in [0, a_max]
+        
+        trans_matrix_n((age_num-1)*a_num_g+1:age_num*a_num_g,1:a_num_g) = repmat(a_prob,a_num_g,1);
+        trans_matrix_n = trans_matrix_n.*(1-exit_vec_n);
+        trans_matrix_n = sparse(trans_matrix_n);
+
+        trans_matrix_o((age_num-1)*a_num_g+1:age_num*a_num_g,1:a_num_g) = repmat(a_prob,a_num_g,1);
+        trans_matrix_o = trans_matrix_o.*(1-exit_vec_n);
+        trans_matrix_o = sparse(trans_matrix_o);
+        
+        dist_n        = m_of_firms_new*ones(1,age_num*a_num_g)/(age_num*a_num_g);
+        dist_o        = m_of_firms_new*ones(1,age_num*a_num_g)/(age_num*a_num_g);
+        dist_ent    = zeros(1,age_num*a_num_g);
+        dist_ent(1:a_num_g) = 1/a_num_g;
+
+        
+        for j=1:1:max_iter
+            dist_new_n    = dist_n *trans_matrix_n;
+            dist_new_n    = dist_new_n + (m_of_firms_new-sum(dist_new_n))*dist_ent;
+            error       = max(abs(dist_new_n-dist_n));
+            if error<dist_tol
+                fprintf("distribution of new converged in %4.1f periods\n",j);
+                break;
+            end
+            dist_n        = dist_new_n;
+        
+        end
+
+        for j=1:1:max_iter
+            dist_new_o    = dist_o *trans_matrix_n;
+            dist_new_o    = dist_new_o + (m_of_firms_new-sum(dist_new_o))*dist_ent;
+            error       = max(abs(dist_new_o-dist_o));
+            if error<dist_tol
+                fprintf("distribution of old converged in %4.1f periods\n",j);
+                break;
+            end
+            dist_o        = dist_new_o;
+        
+        end
+        
+        total_cap   = dist_n * cap_contemp_new(:) + dist_o * cap_contemp_old(:);
+        
+        suply_price = (d_0/total_cap)^(1/e_p);
+        
+        demand_err  = suply_price - p_E;
+        
+        if abs(demand_err)<dem_tol
+            fprintf("demand and supply has converged and the prices is ..." + ...
+                "%2.4f in %2.1f periods\n",p_E,k);
+            break;
+        end
+        
+        p_E     = p_E + 0.2*demand_err/(ceil(k/10));
+    end
+    value_err_n   = a_prob*(v_new_resh_n(1,:))'-c_of_e;
+    value_err_o   = a_prob*(v_new_resh_o(1,:))'-c_of_e;
+    if (abs(value_err_n)<dem_tol||m_of_firms_new<v_tol) && ...
+            (abs(value_err_o)<dem_tol||m_of_firms_old<v_tol)
+        fprintf("entry and exit have converged and E(v_new) and E(v_old) ..." + ...
+            "is %2.4f and %2.4f in %2.1f periods \n"...
+            ,value_err_n,value_err_o,h);
+        break;
+    end
+    if value_err_n>-5
+        m_of_firms_new = m_of_firms_new*(1+0.1*value_err_n);
+    else
+        m_of_firms_new = m_of_firms_new*(1+0.05*value_err_n);
+    end
+    if value_err_o>-5
+        m_of_firms_old = m_of_firms_old*(1+0.1*value_err_o);
+    else
+        m_of_firms_old = m_of_firms_old*(1+0.05*value_err_o);
+    end
+end
+end
