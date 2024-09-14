f43e7fc216625fe38ada318a0cac9185fd2acbe8
ARAzampour <75630126+ARAzampour@users.noreply.github.com>
2024-09-14 14:44:28 +0200

    more updates

--- PP_AoT_coal_gas.m
+++ PP_AoT_coal_gas.m
@@ -54,7 +54,7 @@ trans_t  = 100;
 %%% there can be also an efficiency wedge that has been there without any
 %%% growth; this would be the case for solar and gas
 diff_gr_t       = 10;
-diff_gr         = 0.015;
+diff_gr         = 0.03;
 a_grow          = 0.02;
 % diff_gr_cons    = (1+0.4)^diff_gr_t;    %%% to be used for solar case
 diff_gr_cons    = 1;                    %%% to be used for gas and coal
@@ -108,14 +108,14 @@ e_max       = 30;
 
 
 %%% we have set that generators with a higher initial efficiency would
-%%% depreciate slower, this incorporated using ((1+a_grid).^gamma).^t
+%%% depreciate slower, this incorporated using ((a_grid).^gamma).^t
 
 gamma       = 0.0075;
 alphas      = linspace(0.1,0.95,20);
 
 
 %%% we also incorporate the growth in baseline demand growth
-d0_gr   = 0.013;
+d0_gr   = 0.01;
 %% calibration first part
 %%% the unkown paramteres are d_0, e0_n, e0_o, alpha, c_of_a, c_a_new, rat and
 %%% the moments are p_E, p_e_n, p_e_o, total_eff_ratio, measure_ratio, mean
@@ -153,22 +153,31 @@ vec_c_e_1st     = zeros(100,1);
 %%%%% the fixed costs of new-tech would lead to the increase in their share
 %%%%% Or also use the two-tech system for a case when coal and gas
 %%%%% transition is happening 
-for i=1:1:20
+tracks(15,tracks(15,:)==0)=30;
+track0  = tracks(1:7,tracks(15,:)==min(tracks(15,:)));
+alpha   = track0(1);
+c_of_a  = track0(2); 
+c_a_new = track0(3);
+d_0     = 12;
+e0_n_1st= track0(5)*0.75;
+e0_o    = track0(6)*1.25;
+rat     = track0(7);
+% for i=1:1:20
 tech_dist   = 0.9;
 % for tt = 1:1:100 
-    alpha = alphas(i);
+    % alpha = alphas(i);
     [trans_prob_old,v_new_old,v_new_resh_old,dist_old,trans_matrix_n_1st,p_e_n_1st,cap_contemp_new,eff_n_final,...
         trans_prob_n_1st,v_new_n_1st,v_new_resh_n_1st,dist_n_1st,trans_matrix_old,p_e_o_1st,cap_contemp_old,eff_o_final,...
         age_g,a_grid,a_prob,pi_contemp_new_1st,p_E_old,m_of_firms_new_1st,m_of_firms_old_1st,...
         exit_n_1st,exit_o_1st] = ...
-        Two_tech_ss_AC(a_grow,alpha,a_bar,beta,c_of_a,c_a_new_1st,mu,sigma,a_num_g,age_num,max_iter,...
-        v_tol,dist_tol,fco_o,fco_n,e_p,d_0/tech_dist,c_of_e,c_e_new_1st,dem_tol,tech_dist,...
+        Two_tech_ss_AC(a_grow,alpha,a_bar,beta,rat*c_of_a,rat*c_a_new,mu,sigma,a_num_g,age_num,max_iter,...
+        v_tol,dist_tol,rat*fco_o,rat*fco_n,e_p,d_0/tech_dist,rat*c_of_e,rat*c_e_new_1st,dem_tol,tech_dist,...
         e0_n_1st,e0_o,e_n_eps,e_o_eps,rho,age_reduc,exo_exit,e_max,gamma);
 
     error_ss    = dist_n_1st*cap_contemp_new(:)/(dist_n_1st*cap_contemp_new(:)+dist_old*cap_contemp_old(:))-0.075;
     filensme = ['1st_ss_trial' num2str(i)];
     save(filensme);
-end
+% end
 %     if abs(error_ss)<0.025
 %         break;
 %     else
@@ -198,7 +207,7 @@ end
 ScSz = get(0, 'ScreenSize');
 
 figure(1)
-surf(1+a_grid,age_g,v_new_resh_old);
+surf(a_grid,age_g,v_new_resh_old);
 xlabel('productivity'), ylabel('age')
 title('value of firm in each state')
 set(gca,'Fontsize',32)
@@ -215,7 +224,7 @@ set(gcf,'position',[0,0,ScSz(3),ScSz(4)]);
 figure(6)
 temp            = reshape(trans_prob_old,a_num_g,age_num);
 temp(temp==0)   = NaN;
-surf(age_g,1+a_grid,temp,'edgecolor','none')
+surf(age_g,a_grid,temp,'edgecolor','none')
 % colormap(map_color)
 % shading("interp")
 colorbar
@@ -234,25 +243,25 @@ tech_dist   = (1+diff_gr)^diff_gr_t;
 [trans_prob_o1,v_new_o1,v_new_resh_o1,dist_o1,trans_matrix_n1,p_e_n1,cap_contemp_n21,eff_n_final21,...
     trans_prob_n1,v_new_n1,v_new_resh_n1,dist_n1,trans_matrix_o1,p_e_o1,cap_contemp_o21,eff_o_final21,...
     age_g1,a_grid1,a_prob1,pi_contemp_new1,p_E1,m_of_firms_new1,m_of_firms_old1,exit_n_final1,exit_o_final1] = ...
-    Two_tech_ss_AC(a_grow,alpha,a_bar,beta,c_of_a,c_a_new,mu,sigma,a_num_g,age_num,max_iter,...
-    v_tol,dist_tol,fco_o,fco_n,e_p,d_0*(1+d0_gr)^trans_t/(tech_dist^(1/(1-alpha))),c_of_e,c_e_new,dem_tol,tech_dist,...
+    Two_tech_ss_AC(a_grow,alpha,a_bar,beta,rat*c_of_a,rat*c_a_new,mu,sigma,a_num_g,age_num,max_iter,...
+    v_tol,dist_tol,rat*fco_o,rat*fco_n,e_p,d_0*(1+d0_gr)^trans_t/(tech_dist^(1/(1-alpha))),rat*c_of_e,rat*c_e_new,dem_tol,tech_dist,...
     e0_n_1st,e0_o,e_n_eps,e_o_eps,rho,age_reduc,exo_exit,e_max,gamma);
 
-%% new tech ss (with two techs)
+% new tech ss (with two techs)
 tech_dist   = (1+diff_gr)^diff_gr_t;
 
 [trans_prob_o,v_new_o,v_new_resh_o,dist_o,trans_matrix_n,p_e_n,cap_contemp_n2,eff_n_final2,...
     trans_prob_n,v_new_n,v_new_resh_n,dist_n,trans_matrix_o,p_e_o,cap_contemp_o2,eff_o_final2,...
     age_g,a_grid,a_prob,pi_contemp_new,p_E,m_of_firms_new,m_of_firms_old,exit_n_final,exit_o_final] = ...
-    Two_tech_ss_AC(a_grow,alpha,a_bar,beta,c_of_a,c_a_new,mu,sigma,a_num_g,age_num,max_iter,...
-    v_tol,dist_tol,fco_o,fco_n,e_p,d_0*(1+d0_gr)^trans_t/(tech_dist^(1/(1-alpha))),c_of_e,c_e_new,dem_tol,tech_dist,...
+    Two_tech_ss_AC(a_grow,alpha,a_bar,beta,rat*c_of_a,rat*c_a_new,mu,sigma,a_num_g,age_num,max_iter,...
+    v_tol,dist_tol,rat*fco_o,rat*fco_n,e_p,d_0*(1+d0_gr)^trans_t/(tech_dist^(1/(1-alpha))),rat*c_of_e,rat*c_e_new,dem_tol,tech_dist,...
     e0_n,e0_o,e_n_eps,e_o_eps,rho,age_reduc,exo_exit,e_max,gamma);
 
 save ss_gas_coal
 %%
 % ScSz = get(0, 'ScreenSize');
 % figure(1)
-% surf(1+a_grid,age_g,v_new_resh_n);
+% surf(a_grid,age_g,v_new_resh_n);
 % xlabel('productivity'), ylabel('age')
 % title('value of firm in each state')
 % set(gca,'Fontsize',32)
@@ -260,7 +269,7 @@ save ss_gas_coal
 % 
 % dist_resh = (reshape(dist_n',a_num_g,age_num))';
 % figure(2)
-% surf(1+a_grid,age_g(1:50),(dist_resh(1:50,:)));
+% surf(a_grid,age_g(1:50),(dist_resh(1:50,:)));
 % xlabel('productivity'), ylabel('age')
 % title('mass of firms in each state ')
 % set(gca,'Fontsize',32)
@@ -271,8 +280,8 @@ save ss_gas_coal
 % figure(6)
 % temp            = reshape(trans_prob_n,a_num_g,age_num);
 % temp(temp==0)   = NaN;
-% surf(age_g,1+a_grid,temp,'edgecolor','none')
-% %surface(age_g,1+a_grid,reshape(temp,a_num_g,age_num),'edgecolor','none')
+% surf(age_g,a_grid,temp,'edgecolor','none')
+% %surface(age_g,a_grid,reshape(temp,a_num_g,age_num),'edgecolor','none')
 % % colormap(map_color)
 % % shading("interp")
 % xlabel("age")
@@ -288,8 +297,8 @@ save ss_gas_coal
 % figure(7)
 % temp            = reshape(trans_prob_o,a_num_g,age_num);
 % temp(temp==0)   = NaN;
-% surf(age_g,1+a_grid,temp,'edgecolor','none')
-% % surface(age_g,1+a_grid,reshape(temp,a_num_g,age_num),'edgecolor','none')
+% surf(age_g,a_grid,temp,'edgecolor','none')
+% % surface(age_g,a_grid,reshape(temp,a_num_g,age_num),'edgecolor','none')
 % % colormap(map_color)
 % % shading("interp")
 % xlabel("age")
@@ -305,14 +314,14 @@ save ss_gas_coal
 % 
 % dist_resh = (reshape(dist_o',a_num_g,age_num))';
 % figure(2)
-% surf(1+a_grid,age_g,(dist_resh));
+% surf(a_grid,age_g,(dist_resh));
 % xlabel('productivity'), ylabel('age')
 % title('log of mass of firms in each state ')
 % set(gca,'Fontsize',32)
 % set(gcf,'position',[0,0,ScSz(3),ScSz(4)]);
 % 
 % figure(1)
-% surf(1+a_grid,age_g,v_new_resh_o);
+% surf(a_grid,age_g,v_new_resh_o);
 % xlabel('productivity'), ylabel('age')
 % title('value of firm in each state')
 % set(gca,'Fontsize',32)
@@ -331,8 +340,13 @@ final_val1      = v_new_resh_o1;
 init_dist_o     = dist_old;
 init_dist_n     = dist_n_1st; %%% be aware of the necessary changes when 
                             %%% a one-tech ss
-c_conver        = 10;
+c_conver        = 20;       %%% in the data about 200 w.r.t 500 overhead
 conv_rate       = 0.15;
+diff_gr_cons    = 0.9;
+diff_gr         = 0.04;   %%%% here since the gas generators begin at a disadvantage
+                          %%%% I set their growth rate for transition
+                          %%%% higher so they would catch up with the 2nd
+                          %%%% SS values for the efficiency
 
 penalty_p       = 1; %%% demand is also less elastic in the transition
 penalty_n       = 1;
@@ -340,30 +354,30 @@ penalty_o       = 1; %%% this is the effect of deviation from last year's input
                         %%% for the whole submarket, e.g. a 10% increase in
                         %%% demand would require 20% increase in supply
 
-eff_n_vec           = (((1+a_grid).*alpha*p_E_old/p_e_n_1st.*(((1+a_grid).^gamma)./(1+a_grow)).^age_g)...
+eff_n_vec           = (((a_grid).*alpha*p_E_old/p_e_n_1st.*(((a_grid).^gamma)./(1+a_grow)).^age_g)...
 .^(1/(1-alpha)))';
-eff_n_vec           = min(eff_n_vec,e_max*(1+a_grid)');
-eff_o_vec           = (((1+a_grid).*alpha*p_E_old/p_e_o_1st.*(((1+a_grid).^gamma)./(1+a_grow)).^age_g)...
+eff_n_vec           = min(eff_n_vec,e_max*(a_grid)');
+eff_o_vec           = (((a_grid).*alpha*p_E_old/p_e_o_1st.*(((a_grid).^gamma)./(1+a_grow)).^age_g)...
 .^(1/(1-alpha)))';
-eff_o_vec           = min(eff_o_vec,e_max*(1+a_grid)');
+eff_o_vec           = min(eff_o_vec,e_max*(a_grid)');
 init_input_n = dist_n_1st*eff_n_vec(:)
 init_input_o = dist_old*eff_o_vec(:)
 
 [trans_prob_o_all1,v_new_resh_o_all1,dist_o_all1,measure_vec_o1,p_e_o_vec1,...
     trans_prob_n_all1,v_new_resh_n_all1,dist_n_all1,measure_vec_n1,p_e_n_vec1,...
     age_g1,a_grid1,a_prob1,p_E_vec1,cap_old1,cap_new1] =...
-    MIT_transition_AC(a_grow,alpha,a_bar,beta,c_of_a,c_a_new_vec,mu,a_num_g,age_num,max_iter,...
-    v_tol,dist_tol,fco,e_p,d_0,c_of_e,c_e_new_vec,dem_tol,init_dist_n,init_dist_o,final_val1,final_val2,...
+    MIT_transition_AC(a_grow,alpha,a_bar,beta,rat*c_of_a,rat*c_a_new_vec,mu,sigma,a_num_g,age_num,max_iter,...
+    v_tol,dist_tol,rat*fco_o,rat*fco_n,e_p,d_0,rat*c_of_e,rat*c_e_new_vec,dem_tol,init_dist_n,init_dist_o,final_val1,final_val2,...
     diff_gr,diff_gr_t,init_p_E,final_p_E,trans_t,final_dist_n,final_dist_o,...
     e0_n_vec_1step,e0_o,e_n_eps,e_o_eps,diff_gr_cons,p_e_n1,p_e_n_1st,p_e_o1,p_e_o_1st,...
-    rho,age_reduc,c_conver,conv_rate,exit_n_final1,exit_o_final1,exo_exit,...
+    rho,age_reduc,rat*c_conver,conv_rate,exit_n_final1,exit_o_final1,exo_exit,...
     e_max,gamma,init_input_n,init_input_o,penalty_o,penalty_n,penalty_p,d0_gr);
 
 %%
 %%%% I implement the transition as first the shock of efficiency is
 %%%% observed and after that in the way of transition the shock of loewr
 %%%% input is realized
-diff_gr_cons    = (1+diff_gr)^(diff_gr_t);
+diff_gr_cons    = diff_gr_cons*(1+diff_gr)^(diff_gr_t);
 
 final_dist_o    = dist_o;
 final_dist_n    = dist_n;
@@ -377,24 +391,24 @@ init_dist_n     = dist_n_all1(diff_gr_t,:);
 diff_gr         = 0.0;
 
 
-eff_n_vec           = (((1+a_grid).*alpha*p_E_vec1(diff_gr_t)/p_e_n_vec1(diff_gr_t).*...
-    (((1+a_grid).^gamma)./(1+a_grow)).^age_g).^(1/(1-alpha)))';
-eff_n_vec           = min(eff_n_vec,e_max*(1+a_grid)');
-eff_o_vec           = (((1+a_grid)/diff_gr_cons.*alpha*p_E_vec1(diff_gr_t)/p_e_o_vec1(diff_gr_t).*...
-    (((1+a_grid).^gamma)./(1+a_grow)).^age_g).^(1/(1-alpha)))';
-eff_o_vec           = min(eff_o_vec,e_max*(1+a_grid)');
+eff_n_vec           = (((a_grid).*alpha*p_E_vec1(diff_gr_t)/p_e_n_vec1(diff_gr_t).*...
+    (((a_grid).^gamma)./(1+a_grow)).^age_g).^(1/(1-alpha)))';
+eff_n_vec           = min(eff_n_vec,e_max*(a_grid)');
+eff_o_vec           = (((a_grid)/diff_gr_cons.*alpha*p_E_vec1(diff_gr_t)/p_e_o_vec1(diff_gr_t).*...
+    (((a_grid).^gamma)./(1+a_grow)).^age_g).^(1/(1-alpha)))';
+eff_o_vec           = min(eff_o_vec,e_max*(a_grid)');
 init_input_n = init_dist_n*eff_n_vec(:)
 init_input_o = init_dist_o*eff_o_vec(:)
 
 [trans_prob_o_all,v_new_resh_o_all,dist_o_all,measure_vec_o,p_e_o_vec,...
     trans_prob_n_all,v_new_resh_n_all,dist_n_all,measure_vec_n,p_e_n_vec,...
     age_g,a_grid,a_prob,p_E_vec,cap_old,cap_new] =...
-    MIT_transition_AC(a_grow,alpha,a_bar,beta,c_of_a,c_a_new_vec,mu,a_num_g,age_num,max_iter,...
-    v_tol,dist_tol,fco,e_p,d_0,c_of_e,c_e_new_vec,dem_tol,init_dist_n,init_dist_o,final_val1,final_val2,...
-    diff_gr,diff_gr_t,init_p_E,final_p_E,trans_t,final_dist_n,final_dist_o,...
+    MIT_transition_AC(a_grow,alpha,a_bar,beta,rat*c_of_a,rat*c_a_new_vec,mu,sigma,a_num_g,age_num,max_iter,...
+    v_tol,dist_tol,rat*fco_o,rat*fco_n,e_p,d_0,rat*c_of_e,rat*c_e_new_vec,dem_tol,init_dist_n,init_dist_o,final_val1,final_val2,...
+    diff_gr,diff_gr_t,init_p_E,final_p_E,trans_t-diff_gr_t,final_dist_n,final_dist_o,...
     e0_n_vec,e0_o,e_n_eps,e_o_eps,diff_gr_cons,p_e_n,p_e_n_vec1(diff_gr_t),p_e_o,p_e_o_vec1(diff_gr_t),...
-    rho,age_reduc,c_conver,conv_rate,exit_n_final,exit_o_final,exo_exit,...
-    e_max,gamma,init_input_n,init_input_o,penalty_o,penalty_n,penalty_p);
+    rho,age_reduc,rat*c_conver,conv_rate,exit_n_final,exit_o_final,exo_exit,...
+    e_max,gamma,init_input_n,init_input_o,penalty_o,penalty_n,penalty_p,d0_gr);
 
 
 
@@ -452,7 +466,7 @@ set(gcf,'position',[0,0,ScSz(3),ScSz(4)]);
 %%
 dist_reshaped = (reshape(dist_of_new',a_num_g,age_num))';
 figure(3)
-surf(1+a_grid,age_g,log(dist_reshaped));
+surf(a_grid,age_g,log(dist_reshaped));
 xlabel('productivity'), ylabel('age')
 title('log of mass of firms in each state ')
 set(gca, 'FontSize', 24);
